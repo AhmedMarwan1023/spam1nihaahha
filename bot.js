c7bf2a21016af29e4bbb8cb1f55c275cb4e26c03
AhmedMarwan1023 <44772647+AhmedMarwan1023@users.noreply.github.com>
2018-11-26 22:08:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,88 +1,259 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 const client2 = new Discord.Client();
+const client3 = new Discord.Client();
+const client4 = new Discord.Client();
+const client5 = new Discord.Client();
+const client6 = new Discord.Client();
+const client7 = new Discord.Client();
+
+var prefix = "1";
+var prefix2 = "2";
+var prefix3 = "3";
+var prefix4 = "4";
+var prefix5 = "5";
+var prefix6 = "6";
+var prefix7 = "7";
 
 client.on('ready', () => {
-   console.log(`----------------`);
-   console.log(`Credit Farmm - Script By : Kahrbaa `);
-   console.log(`----------------`);
-   console.log(`Loadinng`);
-   console.log(`Loadinng.`);
-   console.log(`Loadinng..`);
-   console.log(`Loadinng...`);
-   console.log(`This Bots Online ' `);
-   console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);  
+  client.user.setGame(`SPAM ? (1)`,"http://twitch.tv/Dream")
+
 });
 
+client.on('ready', async() => {
+var server = "512113786887340063"; // ايدي السررفر
+var channel = "515918488057937920";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('***FAST IS OP***')
+    },305);
+})
 
 client.on('message', message => {
-    if(message.content === '-راتب'){
-        message.channel.send('#daily')
-    }
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (message.content.startsWith(prefix + 'say')) {
+    if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
+
+
+ client2.on('ready', () => {
+  console.log(`Logged in as ${client2.user.tag}!`);  
+  client2.user.setGame(`SPAM ? (2)`,"http://twitch.tv/Dream")
+
 });
 
-client.on('message', message => {
-    if(message.content === '-مبلغ'){
-        message.channel.send('#credits')
-    }
-});
 
-client.on('message', message => {
-    if(message.content === '-ريب'){
-        message.channel.send('#credits')
-    }
-});
+client2.on('ready', async() => {
+var server = "512113786887340063"; // ايدي السررفر
+var channel = "515918488057937920";//ايدي الروم
+    setInterval(()=>{
+    client2.guilds.get(server).channels.get(channel).send('***FAST IS OP***')
+    },305);
+})
 
 client2.on('message', message => {
-    if(message.content === '-راتب'){
-        message.channel.send('#daily')
-    }
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix2)) return;
+
+  let command2 = message.content.split(" ")[0];
+  command2 = command2.slice(prefix2.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command2 == "say") {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
+
+ client3.on('ready', () => {
+  console.log(`Logged in as ${client3.user.tag}!`);  
+  client3.user.setGame(`SPAM ? (3)`,"http://twitch.tv/Dream")
+
 });
 
-client2.on('message', message => {
-    if(message.content === '-مبلغ'){
-        message.channel.send('#credits')
-    }
+client3.on('ready', async() => {
+var server = "512113786887340063"; // ايدي السررفر
+var channel = "515918488057937920";//ايدي الروم
+    setInterval(()=>{
+    client3.guilds.get(server).channels.get(channel).send('***FAST IS OP***')
+    },305);
+})
+
+client3.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix3)) return;
+
+  let command3 = message.content.split(" ")[0];
+  command3 = command3.slice(prefix3.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command3 == "say") {
+    if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
+
+
+
+
+ client4.on('ready', () => {
+  console.log(`Logged in as ${client4.user.tag}!`);  
+  client4.user.setGame(`SPAM ? (4)`,"http://twitch.tv/Dream")
+
 });
 
-client2.on('message', message => {
-    if(message.content === '-ريب'){
-        message.channel.send('#credits')
-    }
+client4.on('ready', async() => {
+var server = "512113786887340063"; // ايدي السررفر
+var channel = "515918488057937920";//ايدي الروم
+    setInterval(()=>{
+    client4.guilds.get(server).channels.get(channel).send('***FAST IS OP***')
+    },305);
+})
+
+client4.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix4)) return;
+
+  let command4 = message.content.split(" ")[0];
+  command4 = command4.slice(prefix4.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command4 == "say") {
+    if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
+
+
+
+
+ client5.on('ready', () => {
+  console.log(`Logged in as ${client5.user.tag}!`);  
+  client5.user.setGame(`SPAM ? (5)`,"http://twitch.tv/Dream")
+
 });
 
+client5.on('ready', async() => {
+var server = "512113786887340063"; // ايدي السررفر
+var channel = "515918488057937920";//ايدي الروم
+    setInterval(()=>{
+    client5.guilds.get(server).channels.get(channel).send('***FAST IS OP***')
+    },305);
+})
+
+client5.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix5)) return;
+
+  let command5 = message.content.split(" ")[0];
+  command5 = command5.slice(prefix5.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command5 == "say") {
+    if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
 
 
-client.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
-if (message.content === '!spam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
+
+
+
+
+
+
+
+
+
+ client6.on('ready', () => {
+  console.log(`Logged in as ${client6.user.tag}!`);  
+  client6.user.setGame(`SPAM ? (6)`,"http://twitch.tv/Dream")
+
 });
 
-client2.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
-if (message.content === '!spam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
+client6.on('ready', async() => {
+var server = "512113786887340063"; // ايدي السررفر
+var channel = "515918488057937920";//ايدي الروم
+    setInterval(()=>{
+    client6.guilds.get(server).channels.get(channel).send('***FAST IS OP***')
+    },305);
+})
+
+client6.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix6)) return;
+
+  let command6 = message.content.split(" ")[0];
+  command6 = command6.slice(prefix6.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command6 == "say") {
+    if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
+
+ 
+
+
+
+ client7.on('ready', () => {
+  console.log(`Logged in as ${client7.user.tag}!`);  
+  client7.user.setGame(`SPAM ? (7)`,"http://twitch.tv/Dream")
+
 });
 
+client7.on('ready', async() => {
+var server = "512113786887340063"; // ايدي السررفر
+var channel = "515918488057937920";//ايدي الروم
+    setInterval(()=>{
+    client7.guilds.get(server).channels.get(channel).send('***FAST IS OP***')
+    },305);
+})
+
+client7.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix7)) return;
+
+  let command7 = message.content.split(" ")[0];
+  command7 = command7.slice(prefix7.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command7 == "say") {
+    if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
 
 
 
-client.login(process.env.TOKEN);// لا تغير فيها شيء
-client2.login(process.env.TOKEN2);// لا تغير فيها شيء
+
+client.login("");
+client2.login("");
+client3.login("");
+client4.login("");
+client5.login("");
+client6.login("");
+client7.login("");
